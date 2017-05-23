700d181058df878bfa4398deb521620ebfbedd1b
Tibor Benke <ihrwein@gmail.com>
2017-05-23 22:14:12 +0200

    refact(retry): move retry logic into Operation
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/retry.rs
+++ src/retry.rs
@@ -4,65 +4,61 @@ use std::thread;
 use error::Error;
 use backoff::BackOff;
 
-pub trait Operation<P, T> {
-    fn call_op(&self) -> Result<(), Error<P, T>>;
+pub trait Operation<T, E> {
+    fn call_op(&mut self) -> Result<T, Error<E>>;
+
+    fn retry<B>(&mut self, backoff: B) -> Result<T, Error<E>>
+        where B: BackOff
+    {
+        let nop = |_, _| ();
+        self.retry_notify(backoff, nop)
+    }
+
+    fn retry_notify<B, N>(&mut self, mut backoff: B, mut notify: N) -> Result<T, Error<E>>
+        where N: Notify<E>,
+              B: BackOff
+    {
+        backoff.reset();
+
+        loop {
+            let err = match self.call_op() {
+                Ok(v) => return Ok(v),
+                Err(err) => err,
+            };
+
+            let err = match err {
+                Error::Permanent(err) => return Err(Error::Permanent(err)),
+                Error::Transient(err) => err,
+            };
+
+            let next = match backoff.next_back_off() {
+                Some(next) => next,
+                None => return Err(Error::Transient(err)),
+            };
+
+            notify.notify(err, next);
+            thread::sleep(next);
+        }
+    }
 }
 
-impl<P, T, F> Operation<P, T> for F
-    where F: Fn() -> Result<(), Error<P, T>>
+
+impl<T, E, F> Operation<T, E> for F
+    where F: FnMut() -> Result<T, Error<E>>
 {
-    fn call_op(&self) -> Result<(), Error<P, T>> {
+    fn call_op(&mut self) -> Result<T, Error<E>> {
         self()
     }
 }
 
 pub trait Notify<E> {
-    fn notify(&self, err: E, duration: Duration);
+    fn notify(&mut self, err: E, duration: Duration);
 }
 
 impl<E, F> Notify<E> for F
     where F: Fn(E, Duration)
 {
-    fn notify(&self, err: E, duration: Duration) {
+    fn notify(&mut self, err: E, duration: Duration) {
         self(err, duration)
-    }
-}
-
-pub fn retry<O, B, P, T>(operation: O, backoff: B) -> Result<(), Error<P, T>>
-    where O: Operation<P, T>,
-          B: BackOff
-{
-    let nop = |_, _| ();
-    retry_notify(operation, backoff, nop)
-}
-
-pub fn retry_notify<O, B, N, P, T>(operation: O,
-                                   mut backoff: B,
-                                   notify: N)
-                                   -> Result<(), Error<P, T>>
-    where O: Operation<P, T>,
-          N: Notify<T>,
-          B: BackOff
-{
-    backoff.reset();
-
-    loop {
-        let err = match operation.call_op() {
-            Ok(_) => return Ok(()),
-            Err(err) => err,
-        };
-
-        let err = match err {
-            Error::Permanent(err) => return Err(Error::Permanent(err)),
-            Error::Transient(err) => err,
-        };
-
-        let next = match backoff.next_back_off() {
-            Some(next) => next,
-            None => return Err(Error::Transient(err)),
-        };
-
-        notify.notify(err, next);
-        thread::sleep(next);
     }
 }
