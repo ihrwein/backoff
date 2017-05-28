d30f07f5db2af2a1eed246cab3f23c39f3e83df6
Tibor Benke <ihrwein@gmail.com>
2017-05-28 10:22:58 +0200

    docs(examples): add examples
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

---
+++ examples/permanent_error.rs
@@ -0,0 +1,42 @@
+extern crate backoff;
+extern crate reqwest;
+
+use backoff::{Error, ExponentialBackoff, Operation};
+use reqwest::IntoUrl;
+
+use std::fmt::Display;
+use std::io::{self, Read};
+
+fn new_io_err<E: Display>(err: E) -> io::Error {
+    io::Error::new(io::ErrorKind::Other, err.to_string())
+}
+
+fn fetch_url(url: &str) -> Result<String, Error<io::Error>> {
+    let mut op = || {
+        println!("Fetching {}", url);
+        let url = url.into_url()
+            .map_err(new_io_err)
+            // Permanent errors need to be explicitly constucted.
+            .map_err(Error::Permanent)?;
+
+        let mut resp = reqwest::get(url)
+            // Transient errors can be constructed with the ? operator
+            // or with the try! macro. No explicit conversion needed
+            // from E: Error to backoff::Error;
+            .map_err(new_io_err)?;
+
+        let mut content = String::new();
+        let _ = resp.read_to_string(&mut content);
+        Ok(content)
+    };
+
+    let mut backoff = ExponentialBackoff::default();
+    op.retry(&mut backoff)
+}
+
+fn main() {
+    match fetch_url("https::///wrong URL") {
+        Ok(_) => println!("Sucessfully fetched"),
+        Err(err) => panic!("Failed to fetch: {}", err),
+    }
+}
---
+++ examples/retry.rs
@@ -0,0 +1,27 @@
+extern crate backoff;
+extern crate reqwest;
+
+use backoff::{Error, ExponentialBackoff, Operation};
+
+use std::io::Read;
+
+fn fetch_url(url: &str) -> Result<String, Error<reqwest::Error>> {
+    let mut op = || {
+        println!("Fetching {}", url);
+        let mut resp = reqwest::get(url)?;
+
+        let mut content = String::new();
+        let _ = resp.read_to_string(&mut content);
+        Ok(content)
+    };
+
+    let mut backoff = ExponentialBackoff::default();
+    op.retry(&mut backoff)
+}
+
+fn main() {
+    match fetch_url("https://www.rust-lang.org") {
+        Ok(_) => println!("Sucessfully fetched"),
+        Err(err) => panic!("Failed to fetch: {}", err),
+    }
+}
